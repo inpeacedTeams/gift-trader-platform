32a8ad0cafd13ce02e2de7b94ad4efdeef8bea76
inpeacedTeams <egorzzvv@gmail.com>
2026-08-08 01:56:48 +0300

    chore: export user feature models

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,5 +1,6 @@
 from .gifts import Collection, Gift
 from .market import Listing, PriceSnapshot
 from .operations import SourceStatus, Trade
+from .users import AlertEvent, AlertRule, PortfolioWallet, User, WatchlistItem
 
-__all__ = ["Collection", "Gift", "Listing", "PriceSnapshot", "SourceStatus", "Trade"]
+__all__ = ["Collection", "Gift", "Listing", "PriceSnapshot", "SourceStatus", "Trade", "User", "WatchlistItem", "PortfolioWallet", "AlertRule", "AlertEvent"]
