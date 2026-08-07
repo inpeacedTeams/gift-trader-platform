caa9c7c511ee8da9df78bbb30d5fcf65b1ef3f05
inpeacedTeams <egorzzvv@gmail.com>
2026-08-08 02:06:52 +0300

    chore: export portfolio holdings model

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,6 +1,7 @@
 from .gifts import Collection, Gift
 from .market import Listing, PriceSnapshot
 from .operations import SourceStatus, Trade
+from .portfolio import PortfolioHolding
 from .users import AlertEvent, AlertRule, PortfolioWallet, User, WatchlistItem
 
-__all__ = ["Collection", "Gift", "Listing", "PriceSnapshot", "SourceStatus", "Trade", "User", "WatchlistItem", "PortfolioWallet", "AlertRule", "AlertEvent"]
+__all__ = ["Collection", "Gift", "Listing", "PriceSnapshot", "SourceStatus", "Trade", "PortfolioHolding", "User", "WatchlistItem", "PortfolioWallet", "AlertRule", "AlertEvent"]
