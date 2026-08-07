310845406c3d129b98e4f67ffecdbe72e1848338
inpeacedTeams <egorzzvv@gmail.com>
2026-08-08 01:49:27 +0300

    chore: export market snapshot repository

--- app/db/repositories/__init__.py
+++ app/db/repositories/__init__.py
@@ -1,4 +1,5 @@
+from .market_snapshot import MarketSnapshotRepository
 from .price_history import PriceHistoryRepository
 from .source_status import SourceStatusRepository
 
-__all__ = ["PriceHistoryRepository", "SourceStatusRepository"]
+__all__ = ["MarketSnapshotRepository", "PriceHistoryRepository", "SourceStatusRepository"]
