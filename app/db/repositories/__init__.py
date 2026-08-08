8c41d1ab1d6449633ea51323b95d1bed73528f9c
inpeacedTeams <egorzzvv@gmail.com>
2026-08-08 04:49:41 +0300

    fix: export GiftRepository for gifts API

--- app/db/repositories/__init__.py
+++ app/db/repositories/__init__.py
@@ -1,5 +1,6 @@
+from .gifts import GiftRepository
 from .market_snapshot import MarketSnapshotRepository
 from .price_history import PriceHistoryRepository
 from .source_status import SourceStatusRepository
 
-__all__ = ["MarketSnapshotRepository", "PriceHistoryRepository", "SourceStatusRepository"]
+__all__ = ["GiftRepository", "MarketSnapshotRepository", "PriceHistoryRepository", "SourceStatusRepository"]
