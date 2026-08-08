277cf0d74971303097d1569e841a959ca91e3e7e
inpeacedTeams <egorzzvv@gmail.com>
2026-08-08 03:16:50 +0300

    chore: remove superseded migration

--- migrations/versions/0005_alert_notification_delivery.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE alert_events ADD COLUMN IF NOT EXISTS notification_sent_at TIMESTAMPTZ;
-ALTER TABLE alert_events ADD COLUMN IF NOT EXISTS notification_attempts INTEGER NOT NULL DEFAULT 0;
-ALTER TABLE alert_events ADD COLUMN IF NOT EXISTS notification_error TEXT;
-CREATE INDEX IF NOT EXISTS ix_alert_events_delivery ON alert_events(notification_sent_at, notification_attempts);
