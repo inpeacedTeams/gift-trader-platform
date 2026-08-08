6dfe88b53f029c11c1abe39cad4dd26b39cd043a
inpeacedTeams <egorzzvv@gmail.com>
2026-08-08 03:16:44 +0300

    chore: remove superseded migration

--- migrations/versions/0004_portfolio_valuation_provenance.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE portfolio_holdings ADD COLUMN IF NOT EXISTS valuation_source VARCHAR(64) NOT NULL DEFAULT 'unresolved';
-ALTER TABLE portfolio_holdings ADD COLUMN IF NOT EXISTS valuation_confidence NUMERIC(5,2);
