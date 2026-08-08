59ce3fdbc4922d4780f97bc339e6f7ab5a48dbb7
inpeacedTeams <egorzzvv@gmail.com>
2026-08-08 03:26:39 +0300

    chore: export resolver telemetry model

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -2,6 +2,7 @@ from .gifts import Collection, Gift
 from .market import Listing, PriceSnapshot
 from .operations import SourceStatus, Trade
 from .portfolio import PortfolioHolding, PortfolioValuation
+from .resolver import ResolverTelemetry
 from .users import AlertEvent, AlertRule, PortfolioWallet, User, WatchlistItem
 
-__all__ = ["Collection", "Gift", "Listing", "PriceSnapshot", "SourceStatus", "Trade", "PortfolioHolding", "PortfolioValuation", "User", "WatchlistItem", "PortfolioWallet", "AlertRule", "AlertEvent"]
+__all__ = ["Collection", "Gift", "Listing", "PriceSnapshot", "SourceStatus", "Trade", "PortfolioHolding", "PortfolioValuation", "ResolverTelemetry", "User", "WatchlistItem", "PortfolioWallet", "AlertRule", "AlertEvent"]
